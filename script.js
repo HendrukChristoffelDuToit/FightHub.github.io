977c29959261c234a5f0b51d46c7573acf1c6a02
Hendrik <127593594+HendrukChristoffelDuToit@users.noreply.github.com>
2024-05-30 11:59:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+var menuIcon = document.querySelector(".menu-icon");
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+}
